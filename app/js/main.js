7399b741a465e28e9a8bb2f3c8f4ea225c1cb5f4
Alex <sanyanesmiyanov92@gmail.com>
2020-01-16 00:53:28 +0200

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -8,3 +8,29 @@ $(window).scroll(function(event){
    }
    lastScrollTop = st;
 });
+
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Vinnytsia,ua&APPID=42a456b2341ea5ea195bc8c1cc554386').then(function (resp) {return resp.json() }).then(function (data) {
+
+  let dateNow = new Date();
+  let monthes = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  let day = dateNow.getDate(),
+  month = monthes[dateNow.getMonth()],
+  year = dateNow.getFullYear();
+  
+  document.getElementById('dateId').innerHTML = day + ' ' + month + ' ' + year;
+
+    console.log('data ', data);
+    document.getElementById('city-name').textContent = data.name;
+
+    document.getElementById('weather').textContent = Math.round(data.main.temp - 273);
+
+    // document.getElementById('cloud').textContent = data.;
+    // document.getElementById('cloud').textContent = data.;
+    document.getElementById('wind').textContent = data.wind.speed;
+    //Добавляем иконку погоды
+    // document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    })
+    .catch(function () {
+        //Обрабатываем ошибки
+    });
